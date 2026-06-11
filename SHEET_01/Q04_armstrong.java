9741d9a558de1baa99385450a35c1687868c3f85
khushi-7488 <khushiitiwari88@gmail.com>
2026-06-11 14:44:20 +0530

    check for armstrong

---
+++ SHEET_01/Q04_armstrong.java
@@ -0,0 +1,25 @@
+package SHEET_01;
+
+import java.util.*;
+
+public class Q04_armstrong {
+    public static void check(int n) {
+        int num = n;
+        int newNum = 0, digit;
+        while (num != 0) {
+            digit = num % 10;
+            newNum = newNum + (int) (Math.pow(digit, 3));
+            num = num / 10;
+        }
+        if (newNum == n) {
+            System.out.println("Is armstrong");
+        } else {
+            System.out.println("Not an armstrong");
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 153;
+        check(n);
+    }
+}
