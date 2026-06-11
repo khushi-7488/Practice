15d1a8976f10a42ffe9233a9bbabab8b19585072
khushi-7488 <khushiitiwari88@gmail.com>
2026-06-11 18:57:13 +0530

    check for palindrome

---
+++ SHEET_01/Q06_palindrome.java
@@ -0,0 +1,19 @@
+package SHEET_01;
+
+public class Q06_palindrome {
+    public static boolean check(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+        return check(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(check(str, 0, str.length() - 1));
+    }
+}
