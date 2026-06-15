b46eba29f2ade35c00ed1373804764abf8339667
khushi-7488 <khushiitiwari88@gmail.com>
2026-06-15 12:13:13 +0530

    find the sum nth tringular number

---
+++ SHEET_01/Q36_nth_Triagular.java
@@ -0,0 +1,12 @@
+package SHEET_01;
+
+public class Q36_nth_Triagular {
+    public static void main(String[] args) {
+        int n = 4;
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
