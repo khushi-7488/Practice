08702e74b6111aece43717df3d6aa0e65d2f82e7
khushi-7488 <khushiitiwari88@gmail.com>
2026-06-19 19:45:52 +0530

    sum of given string number

---
+++ SHEET_02/Q15_SumOfGivenString.java
@@ -0,0 +1,15 @@
+package SHEET_02;
+
+import java.util.Scanner;
+
+public class Q15_SumOfGivenString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first string");
+        String str1 = sc.nextLine();
+        System.out.println("Enter second string");
+        String str2 = sc.nextLine();
+        int sum = Integer.parseInt(str1) + Integer.parseInt(str2);
+        System.out.println(sum);
+    }
+}
