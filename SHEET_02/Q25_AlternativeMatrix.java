0084efc3ed6278bf6ee121ee90be210446c62532
khushi-7488 <khushiitiwari88@gmail.com>
2026-06-21 00:59:41 +0530

    print 0 and 1 on Alternate matrix element

---
+++ SHEET_02/Q25_AlternativeMatrix.java
@@ -0,0 +1,27 @@
+package SHEET_02;
+
+public class Q25_AlternativeMatrix {
+    public static int[][] find(int n) {
+        int matrix[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = (i + j) % 2;
+            }
+        }
+        return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = find(4);
+        printMatrix(matrix);
+    }
+}
