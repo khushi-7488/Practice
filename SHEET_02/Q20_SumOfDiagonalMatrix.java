069e7230eb833021dcf849007b806b12850d0d21
khushi-7488 <khushiitiwari88@gmail.com>
2026-06-20 21:08:00 +0530

    sum of diagonal elements

---
+++ SHEET_02/Q20_SumOfDiagonalMatrix.java
@@ -0,0 +1,24 @@
+package SHEET_02;
+
+public class Q20_SumOfDiagonalMatrix {
+    public static void find(int matrix[][]) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == j) {
+                    sum += matrix[i][j];
+                } else if (i + j == matrix.length - 1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 } };
+        find(matrix);
+    }
+}
